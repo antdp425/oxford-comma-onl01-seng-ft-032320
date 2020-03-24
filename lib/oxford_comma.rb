5bc45b196a3edf8b9f7cb320ab29f728d3a999f5
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-24 18:10:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
+  if array.length > 2
+    array[-1] = ", and #{array[-1]}"
   elsif array.length == 2
     array.join(" and ")
   else
-    array.collect do |i|
+    array.join
     end
   end
 end
