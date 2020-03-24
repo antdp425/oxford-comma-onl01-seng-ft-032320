7326d30216285eee3eccc136a5bfd0421a5dafe7
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-24 18:06:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,6 @@ def oxford_comma(array)
   array.collect do |i|
     if array.length == 2
       array.join("and")
-    elsif array.index(i) == -1
-      array[-1] = ", and #{i}"
-      array.join
     end
   end
 end
